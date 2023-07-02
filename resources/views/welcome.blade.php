62929868b769c2b43dcd21bfc69c196a90315669
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-02 17:24:27 +0900

    :alembic: ゲートを使ってビューに表示を制限してみる

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -832,11 +832,17 @@
 
 <body class="antialiased">
     @auth
-    <p>
-        {{ Auth::user()->name }}さん、こんにちは。
-    </p>
+        <p
+            class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+            {{ Auth::user()->name }}さん、こんにちは。
+        </p>
 
     @endauth
+    @can('test')
+        <p
+            class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+            ゲートのテスト</p>
+    @endcan
     <div
         class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
         @if (Route::has('login'))
