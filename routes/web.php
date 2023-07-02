9874e4438ab8f73d4f43a2114059b8cdf8b138a9
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-02 15:25:34 +0900

    :alembic: ミドルウェアを使ってみる

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', [TestController::class, 'test'])->name('test');
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('auth');
 Route::get('/post/create', [PostController::class, 'create']);
 
 Route::post('post', [PostController::class, 'store'])->name('post.store');
