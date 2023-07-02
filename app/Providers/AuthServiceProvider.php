c038268924cf2002863cfc84de7ec4135eb60069
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-02 15:55:23 +0900

    :alembic: ゲートでアクセス制限

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+
+
 // use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -21,6 +25,10 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('test', function (User $user) {
+            if ($user->id === 1) {
+                return \true;
+            }
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/test', [TestController::class, 'test'])->name('test');
 Route::get('/', function () {
     return view('welcome');
-})->middleware('auth');
+})->middleware('can:test');
 Route::get('/post/create', [PostController::class, 'create'])->middleware(['auth', 'admin']);
 
 Route::post('post', [PostController::class, 'store'])->name('post.store');
