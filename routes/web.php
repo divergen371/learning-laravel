197051839aadd9ff9f5e7f382f68302727adfd45
divergen371 <51857554+divergen371@users.noreply.github.com>
2023-07-03 12:57:59 +0900

    :sparkles: 編集画面へのルーティング

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,8 @@ Route::get('/post/create', [PostController::class, 'create']);
 Route::post('post', [PostController::class, 'store'])->name('post.store');
 Route::get('post', [PostController::class, 'index']);
 Route::get('/post/show/{post}', [PostController::class, 'show'])->name('post.show');
+Route::get('/post/{post}/edit', [PostController::class, 'edit'])->name('post.edit');
+Route::get('/post/{post}'. [PostController::class, 'update'])->name('post.update');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
